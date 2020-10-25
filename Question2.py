51089d88658b8748439d6ea20e9e8aaeade0d5bf
PythonicNoob <atulsyan850@gmail.com>
2020-10-25 23:56:59 +0530

    Added Question 2

---
+++ Question2.py
@@ -0,0 +1,17 @@
+# Question:
+
+# Write a program to convert temperature in Celsius to Fahrenheit & vice versa.
+
+# Code:
+
+temp = int(input("Temperature in celsius:\n"))
+farhenheit = (temp * 9/5 ) + 32
+print("Temperature in farhenheit:", farhenheit)
+
+# Output:
+#     Temperature in celsius:
+#     0
+#     Temperature in farhenheit: 32.0
+
+# Additional Comments:
+#       None

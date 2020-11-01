b0277aff48c8568a6bbb31976fb8e8acb6a78f36
PythonicNoob <atulsyan850@gmail.com>
2020-11-01 22:29:28 +0530

    Added Question15

---
+++ Question15.py
@@ -0,0 +1,15 @@
+# Question:
+# 15.	Write a program to show if the entered character is uppercase or lowercase
+#
+
+# Code:
+
+print("Lower Case!") if input("Char:\n").islower() else print("Upper case!")
+
+# Output:
+#   Char:
+#   a
+#   Lower Case!
+
+# Additional Comments:
+#   None

c830266fe53ff7c2914b079ad6c224426866fea8
PythonicNoob <atulsyan850@gmail.com>
2020-11-01 22:13:56 +0530

    Updated Q13 to follow the required guidelines. Sometimes even author forgets his own guidlines. LoL

--- Question13.py
+++ Question13.py
@@ -6,3 +6,11 @@
 #
 
 a = input("String to check:\n").lower(); print(a,"is a palindrome") if a == a[::-1] else print(a, "is not a palindrome")
+
+#Input:
+#   String to check:
+#   me
+#   me is not a palindrome
+
+# Additional Comments:
+#   ; in python is used to seperate different statements. if else can be used in this way for simple programs which do not have more than statement

b3434e4dba1e126e5b1038dc1fc0f300929f3154
PythonicNoob <atulsyan850@gmail.com>
2020-11-04 00:33:27 +0530

    Line 17,  was not printing statement correctly. print(f'{i} times {num//i} is {num}.') from print(f'{i} times {num} is {num}.')

--- Question9.py
+++ Question9.py
@@ -14,7 +14,7 @@ if num>1:
     for i in range(2,num):
         if num%i==0:
             print(f'{num} is not a prime number.')
-            print(f'{i} times {num} is {num}.')
+            print(f'{i} times {num//i} is {num}.')
             break
     else:
         print(f'{num} is a prime number.')

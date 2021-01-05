b9901910b9ec94894af33895fe192bb02892c4e8
PythonicNoob <atulsyan850@gmail.com>
2021-01-05 16:02:12 +0530

    Update Question21.py
    Added the answer for Q21

--- Question21.py
+++ Question21.py
@@ -1,2 +1,11 @@
+# Write a program to read two strings and perform concatenation and comparison operations on it.
 
-# DOUBTS
+s1 = input("string 1:\n")
+s2 = input("String 2:\n")
+
+concatenate = input("enter c for concatenate else enter n:\n") == "c"
+
+if concatenate:
+    print(s1+s2)
+else:
+    print("They are", ("same" if s1==s2 else "different"))
